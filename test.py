dcab17433594884f6d812a99b6fdf482b5192d2d
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-05-30 10:27:58 +0700

    upload file exercise pandas

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 
-arr = np.random.rand(4,4)
-print(arr)
-res = np.where(arr>0,arr**2,-2)
+data = pd.DataFrame({'Qu1' : [1,3,4,3,4],
+                    'Qu1' : [2,3,1,2,3],
+                    'Qu2' : [1,5,2,4,4]})
+res = data.value_counts()
 print(res)
