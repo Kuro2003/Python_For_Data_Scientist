90c1b2b27d35311618b221f13152945ac516f832
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-06-26 10:40:07 +0700

    code class exercise

--- test.py
+++ test.py
@@ -1,57 +1,67 @@
-def mergeSort(arr,a,d):
-    if len(arr) > 1:
-
-        mid = len(arr)//2
-
-        L = arr[:mid]
-
-        print(L)
-        R = arr[mid:]
-        
-        d += 1
-        if d == 1:
-            a.append(L)
-        else:
-            a[0] = L
-        print("k = " + str(d))
-        print(a)
-
-        mergeSort(L,a,d)
-        mergeSort(R,a,d)
-
-        i = j = k = 0
-
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
-
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
-
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
-def printList(arr):
-    for i in range(len(arr)):
-        print(arr[i], end=" ")
-    print()
+from os import link
+from scipy.fftpack import tilbert
 
 
-arr = [12, 11, 13, 5, 6, 7]
-d = 0
-a = []
-b= []
-print("Given array is")
-printList(arr)
-mergeSort(arr,a,d)
-print("Sorted array is: ")
-printList(arr)
-print("Chi cuong")
+class Video():
+    def __init__(self,title,link):
+        self.title = title
+        self.link = link
+
+def read_video():
+    title = input("Video title: ")
+    link = input("Video link: ")
+    video = Video(title,link)
+    return video
+
+def print_video(video):
+    print("Video title : ",video.title,end='')
+    print("Video link : ",video.link,end='')
+
+def read_videos():
+    videos = []
+    total = int(input("Enter bumber video : "))
+    for i in range(total):
+        print("Enter video " ,i + 1)
+        video = read_video()
+        videos.append(video)
+    return videos
+
+def print_videos(videos):
+    total = len(videos)
+    print("-----")
+    for i in range(total):
+        print_video(videos[i])
+
+def write_video_to_txt(file,video):
+    file.write(video.title + "\n")
+    file.write(video.link + "\n")
+
+def write_videos_to_txt(videos):
+    total = len(videos)
+    with open("data.txt",'w') as file:
+        file.write(str(len(videos)) + "\n")
+        for i in range(total):
+            write_video_to_txt(file,videos[i])
+
+def read_video_from_txt(file):
+    title = file.readline()
+    link = file.readline()
+    video = Video(title, link)
+    return video
+
+def read_videos_from_txt():
+    videos = []
+    with open('data.txt','r') as file:
+        total = file.readline()
+        for i in range(int(total)):
+            video = read_video_from_txt(file)
+            videos.append(video)
+    return videos
+
+def main():
+    videos = read_videos()
+    write_videos_to_txt(videos)
+    videos = read_videos_from_txt()
+    print_videos(videos)
+
+main()
