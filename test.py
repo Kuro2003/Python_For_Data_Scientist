f567fababf46642159d737d2e3f493aefee35570
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-03 08:49:34 +0700

    exercise handling exception

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-from a import *
-user_1 = int(input("Enter number 1: "))
-user_2 = int(input("Enter number 2: "))
-res = sum_number(user_1,user_2)
-res1 = subtract_number(user_1,user_2)
-print(res)
-print(res1)
-
+while True:
+    try:
+        user_1 = int(input("Enter number 1: "))
+        user_2 = int(input("Enter number 2: "))
+        s = user_1 + user_2
+        print(s)
+        break
+    except:
+        print("ValueError!!!")
